8785f8d0a764699c00c02b8a6c991ef11434a523
Senthil140495 <senthilkumar140495@gmail.com>
2022-10-13 17:44:59 +0530

    code push

---
+++ Arts.java
@@ -0,0 +1,36 @@
+package org.edu;
+
+public class Arts extends Education {
+	
+	public void bSc() {
+		System.out.println("Max strength is 65");
+	}
+	public void bEd() {
+		System.out.println("Max strength is 70");
+	}
+	public void bA() {
+		System.out.println("Max strength is 75");
+	}
+	public void bBa() {
+		System.out.println("Max strength is 80");
+	}
+	
+	@Override
+	public void ug() {
+		System.out.println("Max strength is 85");
+	}
+	
+	@Override
+	public void pg() {
+		System.out.println("Max strength is 90");
+	}
+public static void main(String[] args) {
+	Arts a=new Arts();
+	a.bA();
+	a.bBa();
+	a.bEd();
+	a.bSc();
+	a.ug();
+	a.pg();
+}
+}
---
+++ Education.java
@@ -0,0 +1,11 @@
+package org.edu;
+
+public class Education {
+	public void ug() {
+			System.out.println("Max strength 120 ");
+	}
+	public void pg() {
+			System.out.println("Max strength 60");
+	}
+	
+}
